aef9550f992363cd0c954771f1d9f479861c3c28
Abay Aliyev <113113409+SoftDev-Abay@users.noreply.github.com>
2024-01-26 14:13:43 +0000

    added footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import CardBg from "./CardBg";
+import "./Footer.scss";
+import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <CardBg background="#18191C" cardClassName="footer">
+        <div className="navigation"></div>
+      </CardBg>
+      <CardBg cardClassName="footer-rights">
+        <div className="rights">
+          <div className="text">© 2022. All rights reserved.</div>
+          <div className="social-links-container">
+            <FaFacebook />
+            <FaTwitter />
+            <FaInstagram />
+            <FaYoutube />
+          </div>
+        </div>
+      </CardBg>
+    </>
+  );
 };
 
 export default Footer;
